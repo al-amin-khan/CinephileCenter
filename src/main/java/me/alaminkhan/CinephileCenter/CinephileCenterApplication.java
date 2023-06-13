3e140a4352b45dd3e206ba811efeb350d37a85b2
Al Amin Khan <alaminkhan2010@gmail.com>
2023-06-13 18:33:15 +0600

    app initiate

---
+++ src/main/java/me/alaminkhan/CinephileCenter/CinephileCenterApplication.java
@@ -0,0 +1,13 @@
+package me.alaminkhan.CinephileCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinephileCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinephileCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/alaminkhan/CinephileCenter/CinephileCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alaminkhan.CinephileCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinephileCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
